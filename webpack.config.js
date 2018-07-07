97be42a4a5ef0d73230ec09fbafc1022ce0a32e2
Paulina Lee <0618py@gmail.com>
2018-07-07 01:39:52 -0500

    fix css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: __dirname + "/src/js/main.js",
@@ -8,14 +9,11 @@ module.exports = {
       publicPath: '/'
     },
     module: {
-        rules: [{
+        rules: [
+            {
                 test: /\.scss$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "sass-loader"
-                ]
-            },{
+                use: [MiniCssExtractPlugin.loader, "css-loader"]
+            }, {
                 test: /\.pug$/,
                 loader: 'pug-loader'
             }
@@ -24,6 +22,9 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: __dirname + "/src/templates/index.pug",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "style.css",
         })
     ],
     devServer: {  // configuration for webpack-dev-server
